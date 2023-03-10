39fcf50d5549cea3e678c8981fd4218cc56c655b
NEBULA1978 <rpascualey@gmail.com>
2023-03-10 01:55:11 +0100

    Muestra lista de carpetas y archivos y los copia aqui enesta carpeta

---
+++ Menu-bash-linux-copiar-carpetas.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+# Mostrar las carpetas en la carpeta home del usuario con un número delante para que el usuario pueda seleccionar una
+
+echo "Selecciona una carpeta para copiar:"
+
+select folder in ~/.*; do
+	if [ -z "$folder" ]; then
+		echo "Opción no válida"
+	else
+		break
+	fi
+done
+
+file="$folder"
+
+echo "Introduce la ruta de destino:"
+read destino
+
+cp -r $file $destino
+
+echo "Carpeta $file copiada a $destino"
